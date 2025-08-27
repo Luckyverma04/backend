4e0384dc40927e11ad3d7f33acd888dba2d5b00d
Lucky Verma <luckykishoreverma0404@gmail.com>
2025-08-27 10:42:24 +0530

    utilCodes

--- src/index.js
+++ src/index.js
@@ -8,6 +8,20 @@ connectDB()
     path:'./env'
  })
 
+ connectDB().then(()=>{
+   app.listen(process.env.PORT || 8000,()=>{
+      console.log(`Server is running at port:${process.env.PORT}`);
+   });
+    app.on("error",(error)=>{
+       console.log("Error",error);
+       throw error;
+   })
+ })
+ .catch((err)=>{
+   console.log("Mongo db connection failed!!!",err);
+ })
+
+
 
 
 
